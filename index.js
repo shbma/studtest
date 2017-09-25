c65f7dd51b776d2bb7ff121ce52326a52cc15b33
shbma <web.bma@yandex.ru>
2017-09-25 13:29:33 +0500

    Задачи для Теста-2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 var tasks_blocks_templates = require('./tasks');
 
 var tickets_number = 30; // сколько вариантов делать
+var topic_id = 2; //какую тему обслуживаем
 
 var tickets = []; //будущий список вариантов
 
@@ -13,17 +14,20 @@ for(var t=0; t<tickets_number; t++){
 
     //заполняем вопросы-ответы случайными числами и собираем в список
     tasks_blocks_templates.forEach(function(task_block, i){
-        var task_bone = task_block.tasks[Math.floor(Math.random()*task_block.tasks.length)] //берем случайную задачу из блока
+        if (task_block.topic_id === topic_id){  //некрасиво
 
-        var data = task_bone.data_generator(i); //получаем числа для задания и ответа
+            var task_bone = task_block.tasks[Math.floor(Math.random()*task_block.tasks.length)] //берем случайную задачу из блока
 
-        tasks.push({ 
-            input: ejs.render(task_bone.input, data),
-            answer: ejs.render(task_bone.answer, data)
-        })
+            var data = task_bone.data_generator(i); //получаем числа для задания и ответа
+
+            tasks.push({
+                input: ejs.render(task_bone.input, data),
+                answer: ejs.render(task_bone.answer, data)
+            })
+        }
     })
     
-    tickets.push(tasks);// сохраняем билет
+    if (tasks.length > 0) tickets.push(tasks);// сохраняем билет
 }
 
 //собираем все в красивую страницу
--- tasks.js
+++ tasks.js
@@ -2,6 +2,129 @@
 //Блок должен состоять из близких, но все-же несколько варьирующихся по формулировкам задач.
 module.exports = [
     { //начало блока
+        topic_id: 2,
+        tasks: [ //массив заданий блока
+            {
+                input: `
+                    <p>Записать общее уравнение плоскости проходящей через точку P(<%= input.dot.x %>,<%= input.dot.y %>,<%= input.dot.y %>) перпендикулярно вектору <i>n</i>{<%= input.vector.A %>, <%= input.vector.B %>, <%= input.vector.C %>}
+                    </p>
+                `,
+                answer: `
+                    <p>
+                        <%= answer.plane.A %>x + <%= answer.plane.B %>y + <%= answer.plane.C %>z + <%= answer.plane.D %> = 0
+                    </p>
+                `,
+                data_generator: function(number){ //создает случайный набор входных данных, высчитывает ответ
+                    var input = {
+                        number: number,
+                        dot: {
+                            x: Math.round(Math.random()*10+1),
+                            y: Math.round(Math.random()*10+1),
+                            z: Math.round(Math.random()*10+1)
+                        },
+                        vector: {
+                            A: Math.round(Math.random()*10+1),
+                            B: Math.round(Math.random()*10+1),
+                            C: Math.round(Math.random()*10+1)
+                        }
+                    };
+                    var answer = {
+                        plane: {
+                            A: input.vector.A.toFixed(2),
+                            B: input.vector.B.toFixed(2),
+                            C: input.vector.C.toFixed(2),
+                            D: (-input.vector.A*input.dot.x -input.vector.B*input.dot.y -input.vector.C*input.dot.z).toFixed(2)
+                        }
+                    }
+
+                    return {
+                        input: input,
+                        answer: answer
+                    }
+                }
+            }
+        ]
+    },//конец блока
+    { //начало блока
+        topic_id: 2,
+        tasks: [ //массив заданий блока
+            {
+                input: `
+                    <p>Какие отрезки отсекает на осях координат плоскость <%= input.plane.A %>x + <%= input.plane.B %>y + <%= input.plane.C %>z + <%= input.plane.D %> = 0 ?
+                    </p>
+                `,
+                answer: `
+                    <p> a = <%= answer.a %>, b = <%= answer.b %>, c = <%= answer.c %>                   </p>
+                `,
+                data_generator: function(number){ //создает случайный набор входных данных, высчитывает ответ
+                    var input = {
+                        number: number,
+                        plane: {
+                            A: Math.round(Math.random()*10+1),
+                            B: Math.round(Math.random()*10+1),
+                            C: Math.round(Math.random()*10+1),
+                            D: Math.round(Math.random()*10+1)
+                        }
+                    };
+                    var answer = {
+                        a: (-input.plane.D/input.plane.A).toFixed(2),
+                        b: (-input.plane.D/input.plane.B).toFixed(2),
+                        c: (-input.plane.D/input.plane.C).toFixed(2)
+                    }
+
+                    return {
+                        input: input,
+                        answer: answer
+                    }
+                }
+            }
+        ]
+    },//конец блока
+    { //начало блока
+        topic_id: 2,
+        tasks: [ //массив заданий блока
+            {
+                input: `
+                    <p>Определить, при каком значении <i>r</i> прямые
+                        <%= input.line1.A %>x + <%= input.line1.B %>y + <%= input.line1.C %> = 0 и
+                        r*x + <%= input.line2.B %>y + <%= input.line2.C %> = 0
+                        будут <br>1) коллинеарны <br>2) перпендикулярны
+                    </p>
+                `,
+                answer: `
+                    <p>
+                        1) <%= answer.r_colinear %>
+                        2) <%= answer.r_normal %>
+                    </p>
+                `,
+                data_generator: function(number){ //создает случайный набор входных данных, высчитывает ответ
+                    var input = {
+                        number: number,
+                        line1: {
+                            A: Math.round(Math.random()*10+1),
+                            B: Math.round(Math.random()*10+1),
+                            C: Math.round(Math.random()*10+1)
+                        },
+                        line2: {
+                            A: Math.round(Math.random()*10+1),
+                            B: Math.round(Math.random()*10+1),
+                            C: Math.round(Math.random()*10+1)
+                        }
+                    };
+                    var answer = {
+                        r_colinear: ((input.line2.B / input.line1.B) * input.line1.A).toFixed(2) ,
+                        r_normal: (- input.line1.B * input.line2.B / input.line1.A).toFixed(2)
+                    }
+
+                    return {
+                        input: input,
+                        answer: answer
+                    }
+                }
+            }
+        ]
+    },//конец блока
+    { //начало блока
         topic_id: 1,
         tasks: [ //массив заданий блока
             {
